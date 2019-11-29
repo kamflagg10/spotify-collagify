de38830eeaca919239fe5e861571b92a5d0671a7
Kamira Nowlin-Flagg <kamiranflagg@gmail.com>
2019-11-29 02:46:16 -0500

    both 3x3 and 4x4 grids work for tracks and artists

--- static/user.js
+++ static/user.js
@@ -15,11 +15,22 @@ $(document).ready(function(){
         if (!data.hasOwnProperty('error')){
           artwork = JSON.parse(data);
           var item;
+          var four_id;
+          if ($('#size').val() == "4"){
+            for (item=0; item < artwork.items.length; item++){
+             id = '#four' + item.toString();
+             $(id).attr('src', artwork.items[item].url);
+             console.log(artwork.items[item].url);
+            }
+            $('.four').toggle();
+
+          }
           for (item=0; item < artwork.items.length; item++){
              id = '#' + item.toString();
              $(id).attr('src', artwork.items[item].url);
              console.log(artwork.items[item].url);
            }
+          $('.three').toggle();
         }
         else{
           alert(data.error)
