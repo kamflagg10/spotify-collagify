d28267fe53bc4853998c32fd371a10353afd8bc4
Kamira Nowlin-Flagg <kamiranflagg@gmail.com>
2019-12-01 18:37:13 -0500

    updated user page styling

--- static/user.js
+++ static/user.js
@@ -17,19 +17,22 @@ $(document).ready(function(){
           var item;
           var four_id;
           if ($('#size').val() == "4"){
+          $('.collage-list').empty();
             for (item=0; item < artwork.items.length; item++){
              id = '#four' + item.toString();
              $(id).attr('src', artwork.items[item].url);
+             $('.collage-list').append('<li>' + artwork.items[item].name + '</li>');
              console.log(artwork.items[item].url);
             }
             $('.three').hide();
             $('.four').show();
-
           }
          else{
+         $('.collage-list').empty();
           for (item=0; item < artwork.items.length; item++){
              id = '#' + item.toString();
              $(id).attr('src', artwork.items[item].url);
+             $('.collage-list').append('<li>' + artwork.items[item].name + '</li>');
              console.log(artwork.items[item].url);
            }
           $('.four').hide();
@@ -37,6 +40,7 @@ $(document).ready(function(){
          }
         }
         else{
+          $('.collage-list').empty();
           $('.three').hide();
           $('.four').hide();
           alert(data.error)
