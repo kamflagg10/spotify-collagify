e573763a468c89251010c64c08e18f0b3e69554d
Kamira Nowlin-Flagg <kamiranflagg@gmail.com>
2019-12-02 00:49:57 -0500

    refactoring js file, added display options

--- static/user.js
+++ static/user.js
@@ -15,29 +15,22 @@ $(document).ready(function(){
         if (!data.hasOwnProperty('error')){
           artwork = JSON.parse(data);
           var item;
-          var four_id;
           if ($('#size').val() == "4"){
-          $('.collage-list').empty();
-            for (item=0; item < artwork.items.length; item++){
-             id = '#four' + item.toString();
-             $(id).attr('src', artwork.items[item].url);
-             $('.collage-list').append('<li>' + artwork.items[item].name + '</li>');
-             console.log(artwork.items[item].url);
-            }
+            id = '#four';
             $('.three').hide();
             $('.four').show();
-          }
-         else{
-         $('.collage-list').empty();
-          for (item=0; item < artwork.items.length; item++){
-             id = '#' + item.toString();
-             $(id).attr('src', artwork.items[item].url);
-             $('.collage-list').append('<li>' + artwork.items[item].name + '</li>');
-             console.log(artwork.items[item].url);
            }
-          $('.four').hide();
-          $('.three').show();
-         }
+          else{
+            id = '#';
+            $('.four').hide();
+            $('.three').show();
+          }
+          $('.collage-list').empty();
+          for (item=0; item < artwork.items.length; item++){
+             id += item.toString();
+             $(id).attr('src', artwork.items[item].url);
+             $(id).next().text(artwork.items[item].name)
+            }
         }
         else{
           $('.collage-list').empty();
@@ -52,3 +45,7 @@ $(document).ready(function(){
 
     });
 });
+
+
+
+//$('.collage-list').append('<li>' + artwork.items[item].name + '</li>');
