ad100c15a92b45ab731566ec5699b88c471c9578
Kamira Nowlin-Flagg <kamiranflagg@gmail.com>
2019-12-02 21:48:26 -0500

    user can log out.

--- static/user.js
+++ static/user.js
@@ -1,31 +1,30 @@
-$(document).ready(function(){
-    $('form').on('submit', function(event){
+$(document).ready(function() {
+  $('form').on('submit', function(event) {
 
-        //Request to server
-        $.ajax({
-            data : {
-                collage_type: $('#collage_type').val(),
-                time_frame: $('#time_frame').val(),
-                size: $('#size').val()
-            },
-            type: 'POST',
-            url: '/collage'
-        })
+    //Request to server
+    $.ajax({
+        data: {
+          collage_type: $('#collage_type').val(),
+          time_frame: $('#time_frame').val(),
+          size: $('#size').val()
+        },
+        type: 'POST',
+        url: '/collage'
+      })
 
-        .done(function(data) {
-        if (!data.hasOwnProperty('error')){
+      .done(function(data) {
+        if (!data.hasOwnProperty('error')) {
           artwork = JSON.parse(data);
           var display_style = $('#display').val();
           var item;
 
 
           //Toggle the corresponding collage
-          if ($('#size').val() == "4"){
+          if ($('#size').val() == "4") {
             id = '#four';
             $('.three').hide();
             $('.four').show();
-           }
-          else{
+          } else {
             id = '#';
             $('.four').hide();
             $('.three').show();
@@ -37,36 +36,40 @@ $(document).ready(function(){
           $('.display-text').show();
 
           //Generate collage
-          for (item=0; item < artwork.items.length; item++){
-             $(id + item.toString()).attr('src', artwork.items[item].url);
-             $(id + item.toString()).next().text(artwork.items[item].name);
-             $('.collage-list').append('<li>' + artwork.items[item].name + '</li>');
-            }
-
+          for (item = 0; item < artwork.items.length; item++) {
+            $(id + item.toString()).attr('src', artwork.items[item].url);
+            $(id + item.toString()).next().text(artwork.items[item].name);
+            $('.collage-list').append('<li>' + artwork.items[item].name + '</li>');
+          }
+          console.log("made collage first.")
           //All three options are on initially. Only keep the style from input
           if (display_style == 'overlay')
             $('.collage-list').hide();
-          else if (display_style == 'default'){
+          else if (display_style == 'default') {
             $('.display-text').hide();
             $('.collage-list').hide();
-          }
-          else{
+          } else {
             $('.display-text').hide();
             $('.collage-list').show();
             $('.row').css('margin-left', '500px');
           }
         }
+
         // Error from server
-        else{
+        else {
           $('.collage-list').empty();
           $('.three').hide();
           $('.four').hide();
           alert(data.error);
         }
+      })
+    event.preventDefault();
+  });
 
-        })
-
-        event.preventDefault();
-
-    });
+  //Allow user to logout.
+  $('.logout').click(function() {
+    const url = 'https://www.spotify.com/logout/';
+    const logout_window = window.open(url, 'Spotify Logout', 'width=500,height=500,top=40,left=40');
+    setTimeout(function(){ logout_window.close();}, 3000);
+  });
 });
